d98c685bc0676c00771f471a786ed9f1468930a5
Luke Lai <lukelai.dev@gmail.com>
2023-06-04 19:13:54 +0100

    Use jsx syntax for element

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import RFS from "./RFS/react-from-scratch";
 
-const element = RFS.createElement(
-  "div",
-  { id: "foo" },
-  RFS.createElement("a", null, "bar"),
-  RFS.createElement("b")
+// const element = RFS.createElement(
+//   "div",
+//   { id: "foo" },
+//   RFS.createElement("a", null, "bar"),
+//   RFS.createElement("b")
+// );
+
+/** @jsxRuntime classic */
+/** @jsx RFS.createElement */
+const element = (
+  <div id="foo">
+    <a href=".">bar</a>
+    <br />
+  </div>
 );
 
-console.log(element);
-
-// /** @jsxRuntime classic */
-// /** @jsx RFS.createElement */
-// const element = (
-//   <div id="foo">
-//     <a href=".">bar</a>
-//     <br />
-//   </div>
-// );
 const container = document.getElementById("root");
 RFS.render(element, container);
