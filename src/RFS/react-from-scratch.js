529efaad8992344051b6e0fa492691123fe0a956
Luke Lai <lukelai.dev@gmail.com>
2023-06-06 22:25:35 +0100

    Implement performUnitOfWork function for creating and connecting fibers

--- src/RFS/react-from-scratch.js
+++ src/RFS/react-from-scratch.js
@@ -25,23 +25,30 @@ export function createTextElement(text) {
   };
 }
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   const isProperty = (key) => key !== "children";
 
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
 
-  element.props.children.forEach((child) => render(child, dom));
+  return dom;
+}
 
-  container.appendChild(dom);
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
 }
 
 let nextUnitOfWork = null;
@@ -59,8 +66,56 @@ function workloop(deadline) {
 
 requestIdleCallback(workloop);
 
-function performUnitOfWork(nextUnitOfWork) {
-  //TODO
+function performUnitOfWork(fiber) {
+  // Create new node and append to dom
+  // create the actual DOM node associated with the current fiber if it doesn't already exist.
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  //If the fiber does not have a parent, it means it is the root fiber representing the top-level element being rendered.
+  if (!fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // Create a new fiber for each child
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // Seach for next unit of work. child -> sibling -> uncle(sibling of the parent)
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+
+    nextFiber = nextFiber.parent;
+  }
 }
 
 export default RFS;
