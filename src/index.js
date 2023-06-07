86c79a384af2944437c901a4072fd70f4f743bea
Luke Lai <lukelai.dev@gmail.com>
2023-06-07 13:48:48 +0100

    Implement useState hook for function components

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import RFS from "./RFS/react-from-scratch";
 /** @jsxRuntime classic */
 /** @jsx RFS.createElement */
 function App(props) {
+  const [state, setState] = RFS.useState(1);
   const updateValue = (e) => {
     rerender(e.target.value);
   };
@@ -19,6 +20,8 @@ function App(props) {
       <h1>Hi {props.name}</h1>
       <input onInput={updateValue} value={props.value} />
       <h2>My name is {props.value}</h2>
+      <p>Count:{state}</p>
+      <button onClick={() => setState((c) => c + 1)}>Click</button>
     </div>
   );
 }
